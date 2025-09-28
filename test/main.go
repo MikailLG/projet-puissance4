0c30048fd50cec86794122a8da06ad5267965ec4
Mikail <mikail.liazidgracia@ynov.com>
2025-09-28 17:43:49 +0200

    feat : condition de victoire

--- test/main.go
+++ test/main.go
@@ -7,14 +7,26 @@ import (
 
 func main() {
 	partie := src.InitialiserPartie()
-	src.AfficherGrille(partie.Grille)
-	fmt.Println("\n👉 Joueur 1 joue dans la colonne 4")
-	src.AjouterPion(&partie, 4, partie.Joueur1.Symbole)
-	src.AfficherGrille(partie.Grille)
-	fmt.Println("\n👉 Joueur 2 joue dans la colonne 4")
-	src.AjouterPion(&partie, 4, partie.Joueur2.Symbole)
-	src.AfficherGrille(partie.Grille)
-	fmt.Println("\n👉 Joueur 1 joue dans la colonne 1")
-	src.AjouterPion(&partie, 1, partie.Joueur1.Symbole)
-	src.AfficherGrille(partie.Grille)
+
+	for partie.PartieEnCours {
+		src.AfficherGrille(partie.Grille)
+		fmt.Printf("C'est au tour de %s (%s)\n", partie.JoueurCommence.Nom, partie.JoueurCommence.Symbole)
+
+		var colonne int
+		fmt.Print("Choisissez une colonne (1-7) : ")
+		fmt.Scan(&colonne)
+
+		if !src.AjouterPion(&partie, colonne, partie.JoueurCommence.Symbole) {
+			continue
+		}
+		if src.VerifierVictoire(&partie, partie.JoueurCommence.Symbole) {
+			src.AfficherGrille(partie.Grille)
+			break
+		}
+		if partie.JoueurCommence.Symbole == "X" {
+			partie.JoueurCommence = partie.Joueur2
+		} else {
+			partie.JoueurCommence = partie.Joueur1
+		}
+	}
 }
